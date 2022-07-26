90dbfe58de467d6f99a9fa2458a58476a70aa311
rogerrm95 <rogerrm@live.com>
2022-07-26 15:49:59 -0300

    add: Estrutura da aplicação - Formulário de comentários

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -33,11 +33,23 @@ export function Post() {
                 </p>
 
                 <p>
-                    <a href="#">#novoprojeto {' '}</a>
-                    <a href="#">#nlw {' '}</a>
-                    <a href="#">#rocketseat</a>
+                    <a href="">#novoprojeto</a> {' '}
+                    <a href="">#nlw</a> {' '}
+                    <a href="">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder='Deixe um comentário' />
+
+                <footer>
+                    <button type='submit'>
+                        Publicar
+                    </button>
+                </footer>
+            </form>
         </article>
     )
 }
