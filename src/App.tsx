9a4a1ee804899be86ea9ce3908bb0bbd05253eb5
rogerrm95 <rogerrm@live.com>
2022-07-26 20:57:55 -0300

    add: Os motores do React - Iterando no JSX

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,71 @@ import { Post } from "./components/Post"
 import './styles/global.css'
 import styles from './styles/App.module.css'
 
-function App() {
+export interface Posts {
+  id: number,
+  author: {
+    avatarUrl: string,
+    name: string,
+    role: string
+  },
+  content: Content[],
+  publishedAt: Date
+}
 
+type Content = {
+  type: string,
+  content: string
+}
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/rogerrm95.png",
+      name: 'Rogério Marques',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ], // Markdown * //
+    publishedAt: new Date('2022-07-25 20:00:00')
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: 'Mayke Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ], // Markdown * //
+    publishedAt: new Date('2022-07-26 20:00:00')
+  }
+] as Posts[]
+
+function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
 
       <div className={styles.wrapper}>
-        <SideBar/>
+        <SideBar />
 
         <main>
-          <Post />
-          <Post />
+          {
+            posts.map(post => (
+              <Post 
+                id={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}/>
+            ))
+          }
         </main>
       </div>
     </div>
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,17 +1,20 @@
 import { Avatar } from '../Avatar'
 import { Comment } from '../Comment'
 import styles from './styles.module.css'
+import { Posts } from '../../App'
 
-export function Post() {
+interface PostPros extends Posts { }
+
+export function Post({ author, content, publishedAt }: PostPros) {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar source='https://github.com/rogerrm95.png'/>
+                    <Avatar source={author.avatarUrl} />
 
                     <div className={styles.authorInfo}>
-                        <strong>Rogério Marques</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
@@ -21,24 +24,11 @@ export function Post() {
             </header>
 
             <div className={styles.content}>
-                <p>
-                    Fala galeraa 👋
-                </p>
-
-                <p>
-                    Acabei de subir mais um projeto no meu portifa.
-                    É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                </p>
-
-                <p>
-                    <a href="">jane.design/doctorcare</a>
-                </p>
-
-                <p>
-                    <a href="">#novoprojeto</a> {' '}
-                    <a href="">#nlw</a> {' '}
-                    <a href="">#rocketseat</a>
-                </p>
+                {
+                    content.map(paragraph => (
+                        <p>{paragraph.content}</p>
+                    ))
+                }
             </div>
 
             <form className={styles.commentForm}>
